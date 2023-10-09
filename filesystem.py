edfab28ef1ed2dc4b34dbdfd5f6ff78fee2af5d5
David Valdman <dmvaldman@gmail.com>
2023-10-08 18:00:25 -0700

    Filesystem working

--- filesystem.py
+++ filesystem.py
@@ -1,15 +1,37 @@
+import os
+
 class Filesystem():
-    def __init__(self):
+    extensions = ('.js', '.jsx', '.py', '.json', '.html', '.css', '.scss', '.yml', '.yaml', '.ts', '.tsx', '.ipynb', '.c', '.cc', '.cpp', '.go', '.h', '.hpp', '.java', '.sol', '.sh', '.txt')
+    directory_blacklist = ('build', 'dist', '.github', 'site', 'tests', '.git')
+
+    def __init__(self, curr_dir):
         self.current_folder = None
+        if curr_dir:
+            self.init(curr_dir)
+
+    def init(self, curr_dir):
+        curr_folder = Folder(curr_dir)
+        self.current_folder = curr_folder
+
+        for curr_dir, folders, files in os.walk(curr_dir, topdown=True):
+            curr_folder = self.find_folder(curr_dir)
+            if curr_folder is None:
+                continue
+
+            for file in files:
+                if not file.endswith(self.extensions):
+                    continue
+                file_path = os.path.join(curr_dir, file)
+                curr_folder.add_file(File(file_path))
+
+            for folder in folders:
+                if folder in self.directory_blacklist:
+                    continue
+                folder_path = os.path.join(curr_dir, folder)
+                curr_folder.add_folder(Folder(folder_path))
 
     def read(self):
         return self.current_folder.read()
-
-    def open_all(self):
-        self.current_folder.open_all()
-
-    def close_all(self):
-        self.current_folder.close_all()
 
     def create_folder(self, name, description=''):
         folder = Folder(name, description)
@@ -26,39 +48,55 @@ class Filesystem():
         self.description = description
 
     def find_folder(self, path):
-        if path == self.current_folder.name:
-            return self.current_folder
+        return self.current_folder.find_folder(path)
 
-        folder = self.current_folder
-        rel_path = path.split(self.current_folder.name)[1]
+    def open(self, directory=None):
+        if directory:
+            self.find_folder(directory).open()
+        else:
+            self.current_folder.open()
+        return self
 
-        for name in rel_path.split('/'):
-            if name == '..':
-                folder = folder.parent
-            else:
-                for f in folder.folders:
-                    if f.name == name:
-                        folder = f
-                        break
-
-        return folder
-
-    def open(self):
-        self.current_folder.open()
-
-    def close(self):
-        self.current_folder.close()
+    def close(self, directory=None):
+        if directory:
+            self.find_folder(directory).close()
+        else:
+            self.current_folder.close()
+        return self
 
 class Folder:
-    def __init__(self, name, description=''):
-        self.name = name
+    def __init__(self, path, description=''):
+        self.name = path
         self.description = description
         self.files = []
         self.folders = []
-        self.is_open = False
+        self.is_open = True
 
     def set_description(self, description):
         self.description = description
+
+    def find_folder(self, name):
+        if name == self.name:
+            return self
+
+        for folder in self.folders:
+            if folder.name == name:
+                return folder
+        for folder in self.folders:
+            folder = folder.find_folder(name)
+            if folder is not None:
+                return folder
+        return None
+
+    def find_file(self, name):
+        for file in self.files:
+            if file.name == name:
+                return file
+        for folder in self.folders:
+            file = folder.find_file(name)
+            if file is not None:
+                return file
+        return None
 
     def add_file(self, file):
         self.files.append(file)
@@ -70,23 +108,13 @@ class Folder:
     def open(self):
         self.is_open = True
 
-    def open_all(self):
-        self.open()
-        for folder in self.folders:
-            folder.open_all()
-
     def close(self):
         self.is_open = False
         for folder in self.folders:
             folder.close()
 
-    def close_all(self):
-        self.close()
-        for folder in self.folders:
-            folder.close_all()
-
     def read(self, indent=0):
-        str = '  ' * indent + f'{self.name.upper()}\n'
+        str = '  ' * indent + f'{self.name}: {self.description}\n'
 
         if self.is_open:
             indent += 1
@@ -107,13 +135,14 @@ class File:
         self.description = description
 
     def read(self):
-        return f'{self.name}'
+        return f'{self.name}: {self.description}'
 
 
 if __name__ == '__main__':
     import os
 
-    for root, dirs, files in os.walk('repos/nanoGPT', topdown=True):
-        print(root, dirs, files)
-        for name in files:
-            filename = os.path.join(root, name)
+    directory = "repos/nanoGPT"
+    fs = Filesystem(directory)
+    fs.open()
+    print(fs.read())
+    print('hi')
